00535d9ba358e0289c4439355e52745d8e216345
Siddhesh Zujam <siddheshzujam111@gmail.com>
2025-09-09 21:28:04 +0530

    updated files

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(),
     react()],
-    base: '/Portfolio-Page/',
+    base: '/Portfolio-page/',
 })
